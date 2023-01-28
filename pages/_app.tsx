279831b8a4bb5eb4deb780e37b2209a3f0f22d5c
Jtree03 <wowns0903@gmail.com>
2023-01-28 17:13:03 +0900

    Move Scripts to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 import Layout from "@/components/Layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </NextUIProvider>
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4869276592827991"
+        crossOrigin="anonymous"
+      ></Script>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-657Y3YF0GN"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){ window.dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-657Y3YF0GN');
+        `}
+      </Script>
+      <NextUIProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NextUIProvider>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,6 @@ import Document, {
   DocumentContext,
 } from "next/document";
 import { CssBaseline } from "@nextui-org/react";
-import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -25,23 +24,6 @@ class MyDocument extends Document {
           {CssBaseline.flush()}
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4869276592827991"
-          crossOrigin="anonymous"
-        ></Script>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-657Y3YF0GN"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){ window.dataLayer.push(arguments); }
-          gtag('js', new Date());
-          gtag('config', 'G-657Y3YF0GN');
-        `}
-        </Script>
         <body>
           <Main />
           <NextScript />
